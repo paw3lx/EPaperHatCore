02e974ce58e4919e44a357fe654144b0716f9800
paw3lx <paw3lx@gmail.com>
2019-03-17 19:02:49 +0100

    Added first epaper validation unit test

---
+++ tests/EPaperHatCore.Tests/EpaperTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using BetaSoft.EPaperHatCore;
+using Shouldly;
+using Xunit;
+
+namespace Betasoft.EPaperHatCore.Tests
+{
+    public class EpaperTests
+    {
+        [Fact]
+        public void Width_Or_Height_Less_Or_Equal_Zero_Throws()
+        {
+            var exception = Record.Exception(() => {
+                new Epaper(0, 0);
+            });
+
+            exception.ShouldBeOfType(typeof(InvalidDataException));
+        }
+    }
+}
