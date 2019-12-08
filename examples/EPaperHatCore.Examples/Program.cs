f62480683bd41c17ac82166259f6976947298d26
paw3lx <paw3lx@gmail.com>
2019-12-08 09:22:04 +0100

    Added example

---
+++ examples/EPaperHatCore.Examples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BetaSoft.EPaperHatCore;
+using BetaSoft.EPaperHatCore.GUI;
+using BetaSoft.EPaperHatCore.GUI.Fonts;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+using Unosquare.WiringPi;
+using Unosquare.WiringPi.Native;
+using static BetaSoft.EPaperHatCore.GUI.Enums;
+
+namespace EPaperHatCore.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pi.Init<BootstrapWiringPi>();
+            
+            //initialize ePaper display
+            var ePaper = new Epaper(176, 264);
+            ePaper.Initialize();
+
+            //create black and red screens
+            var blackScreen = new Screen(176, 264, Rotate.ROTATE_270, Color.WHITE);
+            var redScreen = new Screen(176, 264, Rotate.ROTATE_270, Color.WHITE);
+
+            //draw something on screen using a font
+            var font = new Font8();
+            var font2 = new Font20();
+            blackScreen.DrawString(10, 20, "Black text", font, Color.WHITE, Color.BLACK);
+            redScreen.DrawString(10, 50, "Red text", font2, Color.WHITE, Color.RED);
+
+            ePaper.DisplayScreens(blackScreen, redScreen);
+        }
+    }
+}
